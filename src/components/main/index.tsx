c6f260e924b1f2751e2965b274068075b9e236dc
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-11-20 15:51:16 -0300

    fix: solving api call

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,16 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { api } from "../../lib/axios";
 
 interface MainProps {
     search: string | null;
 };
 
+type Characters = {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+        name: string;
+    },
+    location: {
+        name: string;
+    },
+    image: string;
+};
+
 export function Main({
     search,
 }: MainProps): React.JSX.Element {
+
+    const [ characters, setCharacters ] = useState<Characters[]>([]);
+
+    const getAllCharacters = async () => {
+        api.get(`/character`)
+        .then((json) => {
+            const results = json.data.results;
+            setCharacters(results.map((character: Characters) => ({
+                id: character.id,
+                name: character.name,
+                status: character.status,
+                species: character.species,
+                type: character.type,
+                gender: character.gender,
+                origin: {
+                    name: character.origin.name,
+                },
+                location: {
+                    name: character.location.name,
+                },
+                image: character.image,
+            })));
+        })
+    };
+
+    useEffect(() => {
+        getAllCharacters();
+    }, [])
+
     return(
         <View>
             <Text>{search}</Text>
+            {characters.length > 0 ? (
+                characters.map((character: Characters) => (
+                    <Text key={character.id}>{character.name}</Text>
+                ))
+            ) : (
+                <Text>Nenhum personagem encontrado</Text>
+            )}
         </View>
     )
 };
